5c03d1b65c07d8d7ab7f1987f8c876854d543f95
Tanguy <tanguyrygodin@gmail.com>
2015-04-10 10:29:59 +0200

    pre save op for group model => add users

--- server/api/group/group.model.js
+++ server/api/group/group.model.js
@@ -15,36 +15,12 @@ var GroupSchema = new Schema({
 });
 
 GroupSchema
-    .virtual('emails')
-    .set(function(emails){
-        var self=this, mail;
-
-        for(var i=0; i<emails.length; i++){
-            mail=emails[i];
-            User.findOne({email: mail}, {}, function(err, user){
-                if(!user){
-                    self.invitations.push(mail);
-                    console.log('userId non trouvé');
-                }else{
-                    self.users.push(user._id);
-                    //self.__creator;
-                }
-            });
-        }
-        self.save();
-    });
-    
-/*GroupSchema
 .pre('save', function(next){
     var self=this;
-    var emails=self.invitations;
-    for(var i=0; i<emails.length; i++){
-        User.findOne({email: emails[i]}, {}, function(err, user){
-            if(user){
-                self.users.push(user._id);
-            }
-        });
-    }
-    next(); 
-});*/
+    User.find().where('email').in(self.invitations).exec(function(err, users){
+        self.users=users;
+        self.users.push(self.__creator);
+        next();
+    });
+});
 module.exports = mongoose.model('Group', GroupSchema);
